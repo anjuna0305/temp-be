0877c44ab210b066082ba9c2f96911e36ae94411
anjuna0305 <anjunaserasingha@gmail.com>
2024-08-01 15:40:47 +0530

    startup data added

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.ext.asyncio import AsyncSession
 
 load_dotenv()
 
@@ -12,15 +13,19 @@ from app.router.test_router import router as test_router
 from app.config.database.postgres_config import async_engine
 from app.model.db_model import Base
 
-# from app.routers.test_router import router as test_router
-# from app.routers.api_services_router import router as api_router
-# from app.routers.users_router import router as user_router
-# from app.routers.admins_router import router as admin_router
-# from app.routers.outlet_router import router as outlet_router
+from app.config.database.postgres_config import AsyncSessionLocal
+from app.model.db_enum import UserRole
+from app.model.db_model import User
+
 
 app = FastAPI()
 
-origins = ["http://localhost:5173", "http://localhost:*", "http://vh2.local", "http://127.0.0.1:5500"]
+origins = [
+    "http://localhost:5173",
+    "http://localhost:*",
+    "http://vh2.local",
+    "http://127.0.0.1:5500",
+]
 
 app.add_middleware(
     CORSMiddleware,
@@ -30,20 +35,28 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 # Create the database tables on startup
-# @app.on_event("startup")
-# async def init_tables():
-#     async with async_engine.begin() as conn:
-#         await conn.run_sync(Base.metadata.drop_all)
-#         await conn.run_sync(Base.metadata.create_all)
+@app.on_event("startup")
+async def init_tables():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    user = User(
+        username="admin",
+        hashed_password="$2b$12$96axD49e8FMatkcr.9oYouOt.Z7UKFoRmv.ZxlYo4pFJHV8FWcW8y",
+        email="admin@email.com",
+        scopes=UserRole.ADMIN,
+    )
+
+    async with AsyncSessionLocal() as session:
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
 
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(user_router, prefix="/user", tags=["user"])
 app.include_router(admin_router, prefix="/admin", tags=["admin"])
 app.include_router(test_router, prefix="/test", tags=["test"])
-# app.include_router(test_router, prefix="/test", tags=["test"])
-# app.include_router(user_router, prefix="/users", tags=["users"])
-# app.include_router(admin_router, prefix="/admin", tags=["admins"])
-# app.include_router(api_router, prefix="/api", tags=["api_services"])
-# app.include_router(outlet_router, prefix="/outlet", tags=["api_services"])
